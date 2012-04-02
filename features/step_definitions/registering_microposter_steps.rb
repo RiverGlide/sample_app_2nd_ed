b1cb415452da14ca10022228694833d07402fc89
Antony Marcano <antony+github@antonymarcano.com>
2012-04-02 20:20:08 +0100

    All those comments are distracting. Time for them to go -@jamesmartin -@antonymarcano

--- features/step_definitions/registering_microposter_steps.rb
+++ features/step_definitions/registering_microposter_steps.rb
@@ -4,11 +4,8 @@ ADDRESS=GREEDY_CAPTURE
 PROBLEMS=GREEDY_CAPTURE
   
 Given /^I have started registration$/ do
-  #Given I have opened the homepage
   visit '/'
-  #When I click on the 'sign-up' button
   click_on 'Sign up now!'
-  #Then I should see the registration page
   page.should have_content 'Sign up'
   page.should have_button 'Create my account'
 end
@@ -25,23 +22,16 @@ When /^I complete registration with the following:$/ do |table|
     'password confirmation' => 'user_password_confirmation'
   }
   details = table.rows_hash
-  #When I fill in the name with 'andy'
   fill_in registration_page['name'], :with => details['name']
-  #And I fill in the email with 'andy@example.com'
   fill_in registration_page['email'], :with => details['email']
-  #And I fill in the password with 'p4$$wd'
   fill_in registration_page['password'], with: details['password']
-  #And I fill in the confirmation with 'p4$$wd'
   fill_in registration_page['password confirmation'], with: details['password confirmation']
-  #And I click the create my account button
   click_on 'Create my account'
   @user_name = details['name']
 end
 
 Then /^I should see that I am registered$/ do
-  #Then I should see the message 'Welcome to the Sample App!'
   page.should have_css('.alert-success', text: 'Welcome to the Sample App!')
-  #And I should see my username as 'andy'
   page.should have_css('h1', text: @user_name)
 end
 
