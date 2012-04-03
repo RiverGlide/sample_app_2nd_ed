4c1fdd5cab6e79ce79ccdba41dcc3be6b6105701
Antony Marcano <antony+github@antonymarcano.com>
2012-04-03 13:50:43 +0100

    The RegisteringMicroposter, I mean Microblogger, has moved out. -@jamesmartin -@antonymarcano

--- features/step_definitions/registering_microposter_steps.rb
+++ features/step_definitions/registering_microposter_steps.rb
@@ -13,20 +13,6 @@ Before do
   }
 end
 
-module RegisteringMicroposter
-  def start_registration
-    visit '/'
-    click_on 'Sign up now!'
-    page.should have_content 'Sign up'
-    page.should have_button 'Create my account'
-  end
-
-  def i_should_be_registered_as someone
-    page.should have_css('.alert-success', text: 'Welcome to the Sample App!')
-    page.should have_css('h1', text: someone)
-  end
-end
-
 def complete the_form, details
   fill_in the_form['name'], with: details['name']
   fill_in the_form['email'], with: details['email']
@@ -67,7 +53,7 @@ def for_these things
 end
 alias with_these for_these
 
-World(RegisteringMicroposter)
+World(RegisteringMicroblogger)
 
 Given /^I have started registration$/ do
   start_registration
